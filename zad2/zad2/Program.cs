aec181403ccbf7d8f2ec0b9582c5d6cdd89365bb
Niksior <heroes.VS.misio@gmail.com>
2018-05-07 17:29:52 +0200

    Almost end

---
+++ zad2/zad2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Program
+    {
+        private Tablica table = new Tablica();
+
+        public Program()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            Console.ForegroundColor = colors[12];
+            Console.Title = "Tabbed";
+            Program program = new Program();
+            program.ShowMenu();
+ 
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Hello");
+            Console.WriteLine("I am your int table");
+            Console.WriteLine("Choose and option");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("1 - show value");
+            Console.WriteLine("2 - write value on index");
+            Console.WriteLine("3 - add value on the end");
+            Console.WriteLine("---------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zad2/zad2/Tablica.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Tablica
+    {
+        private int[] table;
+        private int lastIndex;
+        private int defaultValue = 6;
+
+        public Tablica()
+        {
+            table = new int[3];
+            FillTable(table);
+            lastIndex = 0;
+        }
+
+        public void ReadValue(int i)
+        {
+            if(i < table.Length && i >= 0)
+            {
+                Console.WriteLine("Value = " + table[i]);
+            } else
+            {
+                throw new OutOfRange();
+            }
+                
+        }
+
+        public void WriteValue()
+        {
+            Console.WriteLine("Provide index number, your actual table range is 0-" + (table.Length - 1));
+            String tmp;
+            tmp = Console.ReadLine();
+            int index = Int32.Parse(tmp);
+            if (index < 0)
+                throw new OutOfRange();
+            if (index > table.Length - 1)
+            {
+                this.lastIndex = index;
+                ResizeTable(index + 1);
+            }
+            Console.WriteLine("Provide value, it will be written on position [" + index + "]");
+            tmp = Console.ReadLine();
+            int parsed = Int32.Parse(tmp);
+            table[index] = parsed;
+            Console.WriteLine("Value: " + parsed + " saved");
+        }
+
+        private void FillTable(int[] tmp)
+        {
+            for (int i = 0; i < tmp.Length; i++)
+                tmp[i] = defaultValue;
+        }
+
+        public int getLastIndex()
+        {
+            return this.lastIndex;
+        }
+
+        public int[] GetTable()
+        {
+            return this.table;
+        }
+
+        private void ResizeTable(int newSize)
+        {
+            int[] tmp = new int[newSize];
+            FillTable(tmp);
+            for (int i = 0; i < table.Length; i++)
+                tmp[i] = table[i];
+            this.table = tmp;
+        }
+
+        public void Add()
+        {
+            if(table.Length - 1 == lastIndex)
+            {
+                ResizeTable(table.Length * 2);
+            }
+            Console.WriteLine("Provide the value to add on the ond of table");
+            int value = Int32.Parse(Console.ReadLine());
+            lastIndex++;
+            table[lastIndex] = value;
+        }
+
+    }
+}
